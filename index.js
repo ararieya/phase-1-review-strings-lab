02a40d1554278f9f96f831ae733cae5805e8daeb
Ashley Rarieya <ararieya@gmail.com>
2023-09-18 15:48:52 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -5,6 +5,16 @@ console.log (welcomeMessage);
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
 console.log(excitedWelcomeMessage);
 
-const shortGreeting= `Welcome, ${currentUser}`;
-console.log(shortGreeting)
+console.log ('Edsger Dijkstra'.charAt(0));
+console.log ('Edsger Dijkstra'[0]);
+console.log ('Edsger Dijkstra'.length);
+console.log('Edsger Dijkstra'[15]);
+console.log('Edsger Dijkstra'[14]);
+console.log('Edsger Dijkstra'.slice());
+console.log('Edsger Dijkstra'.slice(7));
+console.log('Edsger Dijkstra'.slice(0,3));
 
+console.log (currentUser.slice(0,1))
+
+const shortGreeting= `Welcome, ${currentUser.slice(0,1)}!`;
+console.log(shortGreeting)
